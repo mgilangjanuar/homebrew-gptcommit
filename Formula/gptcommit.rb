b0158625838420663c2a9f21ed19e8b11af12ceb
M Gilang Januar <mgilangjanuar@gmail.com>
2023-03-25 09:48:40 +0700

    update sha256

--- Formula/gptcommit.rb
+++ Formula/gptcommit.rb
@@ -2,7 +2,7 @@ class Gptcommit < Formula
   desc "gptcommit is a tool to help you commit your changes to a git repository with ChatGPT API"
   homepage "https://github.com/mgilangjanuar/gptcommit"
   url "https://github.com/mgilangjanuar/gptcommit/releases/download/v0.1.22/gptcommit.tar.gz"
-  sha256 "9510ebd66365effded41f83a1889d16f8910a8b27efa76b2a4fd3b4b369605bf"
+  sha256 "ffc9e2c0ecf27a75692833d25fcf64383c256bf5fb24e258f8ba7780791f005e"
   license "MIT"
   version "0.1.22"
 
