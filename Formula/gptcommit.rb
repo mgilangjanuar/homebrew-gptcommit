83477f99bdb9ffe701d664be2a68fadafc72f18d
M Gilang Januar <mgilangjanuar@gmail.com>
2023-03-25 09:49:43 +0700

    update version

--- Formula/gptcommit.rb
+++ Formula/gptcommit.rb
@@ -4,7 +4,7 @@ class Gptcommit < Formula
   url "https://github.com/mgilangjanuar/gptcommit/releases/download/v0.1.22/gptcommit.tar.gz"
   sha256 "ffc9e2c0ecf27a75692833d25fcf64383c256bf5fb24e258f8ba7780791f005e"
   license "MIT"
-  version "0.1.22"
+  version "0.1.23"
 
   def install
     bin.install "gptcommit"
