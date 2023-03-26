97211f2ae2c8e2b9ff00c789c9096353af9b0d0f
M Gilang Januar <mgilangjanuar@gmail.com>
2023-03-26 10:47:21 +0700

    update to 0.1.24

--- Formula/gptcommit.rb
+++ Formula/gptcommit.rb
@@ -1,10 +1,10 @@
 class Gptcommit < Formula
   desc "gptcommit is a tool to help you commit your changes to a git repository with ChatGPT API"
   homepage "https://github.com/mgilangjanuar/gptcommit"
-  url "https://github.com/mgilangjanuar/gptcommit/releases/download/v0.1.23/gptcommit.tar.gz"
-  sha256 "ffc9e2c0ecf27a75692833d25fcf64383c256bf5fb24e258f8ba7780791f005e"
+  url "https://github.com/mgilangjanuar/gptcommit/releases/download/v0.1.24/gptcommit.tar.gz"
+  sha256 "86e8a96a161c92b25a7004714343ba238e7e7acc2e3e0db63cc14d8d9794593f"
   license "MIT"
-  version "0.1.23"
+  version "0.1.24"
 
   def install
     bin.install "gptcommit"
